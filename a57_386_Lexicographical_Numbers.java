3556a99888745f038d4ebfbbc429dec4946be3c5
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2024-09-21 21:15:23 +0530

    Create a57_386_Lexicographical_Numbers.java

---
+++ a57_386_Lexicographical_Numbers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+         List<Integer> list = new ArrayList<>();
+         counting(0, n,list);
+         return list;
+    }
+     public static void counting(int curr, int n, List<Integer> list) {
+        if (curr > n) {
+			return;
+		}
+        if(curr!=0)
+		list.add(curr);
+
+		int i = 0;
+		if (curr == 0) {
+			i = 1;
+		}
+
+		for (; i <= 9; i++) {
+			counting(curr * 10 + i, n,list);
+		}
+
+	}
+}
