8719d9a1a26eb05d756eb05af1253b09ea777943
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2024-12-18 23:21:46 +0530

    Create a93_1475. Final Prices With a Special Discount in a Shop.java

---
+++ a93_1475. Final Prices With a Special Discount in a Shop.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[n]; 
+        for (int i = n - 1; i >= 0; --i) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                res[i] = prices[i];
+            } else {
+                res[i] = prices[i] - prices[stack.peek()];
+            }
+            stack.push(i);
+        }
+        
+        return res;
+    }
+}
